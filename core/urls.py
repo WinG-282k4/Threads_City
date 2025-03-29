bc2baeae1d80c5df69d9c16bef0a209093dd018a
WinG 2k4 <npthanh60@gmail.com>
2025-03-29 16:42:23 +0700

    add eror URL

--- core/urls.py
+++ core/urls.py
@@ -15,13 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 
+def custom_404(request):
+    return JsonResponse({
+        "status": "error",
+        "message": "Endpoint không tồn tại. Vui lòng kiểm tra lại URL."
+    }, status=404)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('thread.urls')),
     path('api/auth/', include('accounts.urls')),
+    re_path(r'^.*$', custom_404),  # Catch all URLs
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
